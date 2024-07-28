716a6bc23a7d13ce9c0add8527836288f6062d11
pawan <pavandeore90@gmail.com>
2024-07-28 10:08:13 +0530

    update code added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mysql = require("mysql2");
 
 const app = express();
+app.use(express.json());
 const port = 9000;
 
 const db = mysql.createConnection({
@@ -32,6 +33,37 @@ app.get("/records", (req, res) => {
     }
 
     res.json(results);
+  });
+});
+
+app.put("/update-records", (req, res) => {
+  const newName = req.body.name; // assumptions optverified = false - (the field that we want to set false for all users)
+  db.beginTransaction((err) => {
+    if (err) {
+      console.error("transaction error");
+      return res.status(500).json({ error: "transaction error" });
+    }
+
+    const query = "UPDATE random_table SET name = ?";
+    db.query(query, [newName], (error, results) => {
+      if (error) {
+        return db.rollback(() => {
+          console.error("query error");
+          res.status(500).json({ error: "query error" });
+        });
+      }
+
+      db.commit((err) => {
+        if (err) {
+          return db.rollback(() => {
+            console.error("commit error", err);
+            res.status(500).json({ error: "commit error" });
+          });
+        }
+
+        res.json({ success: true, results });
+      });
+    });
   });
 });
 
